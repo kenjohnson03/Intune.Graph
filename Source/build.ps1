f91869c0b8e166a77c1f76c3bfe696be3c30dfcd
Ken Johnson <no email>
2023-11-12 11:09:37 -0600

    Add NuGet API key parameter and options to skip updating README.md and publishing module to PSGallery

--- Source/build.ps1
+++ Source/build.ps1
@@ -1,20 +1,44 @@
+param (
+    [Parameter(Mandatory, ParameterSetName="Publish", HelpMessage="NuGet API Key")]
+    [string]$NUGET_API_KEY,
+    [Parameter(Mandatory, ParameterSetName="SkipReadMe", HelpMessage="Skip updating README.md")]
+    [Parameter(ParameterSetName="SkipPublishModule", HelpMessage="Skip updating README.md")]
+    [switch]$SkipReadMe,
+    [Parameter(Mandatory, ParameterSetName="SkipPublishModule", HelpMessage="Skip publishing module to PSGallery")]
+    [Parameter(ParameterSetName="SkipReadMe", HelpMessage="Skip publishing module to PSGallery")]
+    [switch]$SkipPublishModule
+)
+
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
 # Update Module Manifest
-$ModuleManifestPath = "$scriptPath\IntuneGraph.psd1"
+$ModuleManifestPath = "$scriptPath\Intune.Graph.psd1"
 $ModuleManifest = Import-PowerShellDataFile $ModuleManifestPath
 $ModuleVersion = $ModuleManifest.ModuleVersion.Split(".")
 $UpdatedModuleVersion = "{0}.{1}.{2}" -f $ModuleVersion[0],$ModuleVersion[1],([int]$ModuleVersion[2]+1)
 Update-ModuleManifest -Path $ModuleManifestPath -ModuleVersion $UpdatedModuleVersion
 
+if($false -eq $SkipReadMe)
+{
+    # Update README.md
+    $ReadmePath = "$scriptPath\..\README.md"
+    $Readme = Get-Content $ReadmePath
+    $Readme[0] = "# Intune.Graph"
+    $Readme[1] = "Last Updated: $((Get-Date).ToString('MM/dd/yyyy')) <br/>"
+    $Readme[2] = "Last Updated By: $(git log -1 --pretty=%an) <br/>"
+    $Readme | Set-Content $ReadmePath
+}
+
 # Run tests TODO
 
 # Build module
-New-Item -ItemType Directory -Force -Path "$scriptPath\bin"
-$BuildPath = "$scriptPath\bin\IntuneGraph\$UpdatedModuleVersion"
-New-Item -ItemType Directory -Force -Path $BuildPath
-Copy-Item -Path "$scriptPath\IntuneGraph.psd1" -Destination $BuildPath
-Copy-Item -Path "$scriptPath\IntuneGraph.psm1" -Destination $BuildPath
+$BuildPath = "$scriptPath\bin\Intune.Graph\$UpdatedModuleVersion"
+New-Item -ItemType Directory -Force -Path $BuildPath | Out-Null
+Copy-Item -Path "$scriptPath\Intune.Graph.psd1" -Destination $BuildPath
+Copy-Item -Path "$scriptPath\Intune.Graph.psm1" -Destination $BuildPath
 
-# Publish module
-Publish-Module -Path $BuildPath -NuGetApiKey $env:NUGET_API_KEY -Repository PSGallery -Force
+if($false -eq $SkipPublishModule)
+{
+    # Publish module
+    Publish-Module -Path $BuildPath -NuGetApiKey $NUGET_API_KEY -Repository PSGallery -Force
+}
